d04adb080768396fe45014c71a59f2af24df3d19
StelianLuparu <stelian_luparu@yahoo.com>
2021-02-13 10:53:16 +0200

    modified settings.py and README.md

--- appointment_tracker/appointment_tracker/settings.py
+++ appointment_tracker/appointment_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appointments',  # adaugat de mine deoarece am uitat
 ]
 
 MIDDLEWARE = [
