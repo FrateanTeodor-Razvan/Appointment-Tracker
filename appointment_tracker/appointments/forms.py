c374d5352edb8f483463d545b00a89abf2902170
StelianLuparu <stelian_luparu@yahoo.com>
2021-02-18 13:21:55 +0200

    modified Appointments forms.py

--- appointment_tracker/appointments/forms.py
+++ appointment_tracker/appointments/forms.py
@@ -2,10 +2,15 @@ from django import forms
 
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password_repeat = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    phone_number = forms.CharField(widget=forms.NumberInput(attrs={'class': 'form-control'}), required=False)
+    username = forms.CharField(max_length=20, widget=forms.TextInput)
+    email = forms.EmailField(max_length=100, widget=forms.EmailInput)
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput)
+    password_repeat = forms.CharField(max_length=20, widget=forms.PasswordInput)
+    first_name = forms.CharField(max_length=20, widget=forms.TextInput)
+    last_name = forms.CharField(max_length=20, widget=forms.TextInput)
+    phone_number = forms.CharField(max_length=1, widget=forms.NumberInput, required=False)
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=20, widget=forms.TextInput)
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput)
